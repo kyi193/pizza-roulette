3b1cfaf3fcf0410589586fb45c90c9c3af3533d2
Kevin <kevin.yi93@gmail.com>
2020-07-29 20:50:30 -0400

    Create GestureWheelSpinner unit test file and import packages/components

---
+++ __tests__/GestureWheelSpinnerTest.js
@@ -0,0 +1,5 @@
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import GestureSpinnerWheel from '../components/GestureSpinnerWheel'
+
+Enzyme.configure({ adapter: new Adapter() })
