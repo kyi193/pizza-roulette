b1934e9cd30c8c7992443c5af3ba4bcc7cd12cc8
Kevin <kevin.yi93@gmail.com>
2020-07-29 21:03:59 -0400

    Create unit test to verify <GestureSpinnerWheel> renders

--- __tests__/GestureWheelSpinnerTest.js
+++ __tests__/GestureWheelSpinnerTest.js
@@ -3,3 +3,11 @@ import Adapter from 'enzyme-adapter-react-16'
 import GestureSpinnerWheel from '../components/GestureSpinnerWheel'
 
 Enzyme.configure({ adapter: new Adapter() })
+
+describe("GestureSpinnerWheel component", () => {
+  test("renders", () => {
+    const wrapper = shallow(<GestureSpinnerWheel />)
+
+    expect(wrapper.exists()).toBe(true)
+  })
+})
